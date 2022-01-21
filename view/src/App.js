d2db551196fe173123bb2007521cc979b1b40471
thepurplesnail <vanessaman446@gmail.com>
2022-01-21 01:46:55 -0500

    moved Bootrap cdn from App -> Index

--- view/src/App.js
+++ view/src/App.js
@@ -6,10 +6,6 @@ import React, {useState, useEffect} from 'react';
 function App() {
   return (
     <div id = "App" className = 'container-fluid'>
-      <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
-      </head>
-      <body>
         <BrowserRouter>
           <Routes>
             <Route path="*" element = {<Navigate to = "/catalog/*" />} />
@@ -17,7 +13,6 @@ function App() {
             <Route path="/catalog/*" element={<Sidebar/>} />
           </Routes>
         </BrowserRouter>
-      </body>
       <Outlet/>
     </div>
   );
