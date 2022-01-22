8a8d8fed4cf7add578898de8c04ce2b7802ffc96
thepurplesnail <vanessaman446@gmail.com>
2022-01-22 11:30:35 -0500

    removed div container

--- view/src/components/icons/status.js
+++ view/src/components/icons/status.js
@@ -1,15 +1,13 @@
 
 export default function Status (props) {
     return(
-        <div className = {props.stat === 'Available' ? 'bg-success' : props.stat === 'Maintenance' ? 'bg-danger' : 'bg-warning'} 
+        <text className = {props.stat === 'Available' ? 'bg-success' : props.stat === 'Maintenance' ? 'bg-danger' : 'bg-warning'} 
             style = {{width: 'fit-content', 
                     paddingLeft: '.5rem', paddingRight: '.5rem', 
-                    borderRadius: '10px', textAlign: 'center'
+                    borderRadius: '10px', textAlign: 'center', color: 'white'
                 }}
         >
-            <text style = {{color: 'white'}}>
                 {props.stat}
-            </text>
-        </div>
+        </text>
     )
 }
