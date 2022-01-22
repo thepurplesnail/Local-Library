b50af011e1273dd451942a9d61b98b67d629309c
thepurplesnail <vanessaman446@gmail.com>
2022-01-22 00:53:47 -0500

    added back arrow to books page

--- view/src/components/books.js
+++ view/src/components/books.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, {useState, useEffect} from 'react'
 import { Route, Link } from 'react-router-dom'
+import Btn from './icons/btn.js'
 
 export default function Books() {
   const [post, setPost] = useState(null);
@@ -13,23 +14,24 @@ export default function Books() {
   }, [post]);
 
   if (!post) return null;
-  
+
   return ( 
-    <div className = 'mainContainer'>
-      <main style={{ padding: "1rem" }}>
-        <h1>Books</h1>
-        <ul style = {{marginTop: "1.5rem" }}>
-          {post.map(
-            book => 
-            <li key={book.id}>
-              <Link to = {`/catalog/book/${book.id}`} className = "text-decoration-none">
-                {book.title}
-              </Link> ({book.author.family_name}, {book.author.first_name})
-            </li>
-            )
-          } 
-        </ul>
-      </main>
+    <div className = 'btn-pg-container'>
+      <div style = {{padding: '1rem'}}><Btn url = '/catalog'/></div>
+      <div style = {{padding: '.2rem'}}>
+          <h1>Books</h1>
+          <ul style = {{marginTop: '1.5rem'}}>
+            {post.map(
+              book => 
+              <li key={book.id}>
+                <Link to = {`/catalog/book/${book.id}`} className = "text-decoration-none">
+                  {book.title}
+                </Link> ({book.author.family_name}, {book.author.first_name})
+              </li>
+              )
+            } 
+          </ul>
+      </div>
     </div>
 
   );
