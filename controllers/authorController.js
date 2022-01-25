23ff5c692fef9ff4bdd1c203c25052ad52ab2587
thepurplesnail <vanessaman446@gmail.com>
2022-01-25 16:07:32 -0500

    author form loads and handles errors

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
-var Author = require('../models/author');
+var Author = require('../models/author')
 var Book = require('../models/book')
-var sequelize = require('../database');
+var sequelize = require('../database')
+var {check, body, validationResult} = require('express-validator')
 
 Promise.resolve().then(sequelize.auth());
 
@@ -25,15 +26,35 @@ exports.author_detail = function(req, res) {
     .then(results => res.json(results));
 };
 
-// Display Author create form on GET.
-exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
-};
-
 // Handle Author create on POST.
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+    // Validate and sanitize fields.
+    check('first_name').trim().isLength({ min: 1 }).escape().withMessage('First name must be specified.')
+    .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    check('family_name').trim().isLength({ min: 1 }).escape().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    check('date_of_birth', 'Invalid date of birth').optional({checkFalsy: true}).isISO8601().toDate().escape(),
+    check('date_of_death', 'Invalid date of death').optional({checkFalsy: true}).isISO8601().toDate().escape(),
+    
+    (req, res) => {
+        
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) res.json(errors);
+        else {
+            let dateFix = date => date ? date : null;
+            let author = {
+                first_name: req.body.first_name,
+                family_name: req.body.family_name,
+                date_of_birth: dateFix(req.body.date_of_birth),
+                date_of_death: dateFix(req.body.date_of_death)
+            };
+            Author.create(author);
+            console.log('>>>>> Author successfully added!')
+            res.json('Author successfully added!');
+        }
+    }
+    
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = function(req, res) {
--- view/src/components/forms/create_author.js
+++ view/src/components/forms/create_author.js
@@ -1,9 +1,81 @@
+import Btn from '../icons/btn'
+import {useState} from 'react'
+import axios from 'axios'
+
+
 export default function CreateAuthor(){
+    const[first, setFirst] = useState('');
+    const[last, setLast] = useState('');
+    const[DoB, setDoB] = useState('');
+    const[DoD, setDoD] = useState('');
+
+    const handleChangeFirst = e => setFirst(e.target.value);
+    const handleChangeLast = e => setLast(e.target.value);
+    const handleChangeDoB = e => setDoB(e.target.value);
+    const handleChangeDoD = e => setDoD(e.target.value);
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post('http://localhost:5000/catalog/author/create', 
+        {
+            first_name : first,
+            family_name: last,
+            date_of_birth: DoB, 
+            date_of_death: DoD
+        })
+        .then(res => {
+            if (res.data.errors) alert(res.data.errors[0].msg);
+            else alert(res.data);
+        });
+        setFirst('');
+        setLast('');
+        setDoB('');
+        setDoD('');
+    }
+
     return(
-        <div className="col-sm-10">
-            <main style={{ padding: "1rem" }}>
-                <h2>Create author</h2>
-            </main>
+        <div className="btn-pg-container">
+            <div className = 'btn'><Btn url = '/catalog'/></div>
+            <div>
+                <h1>Create author</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className = 'label'>
+                        <label><strong>First name:</strong></label>
+                    </div>
+                    <div>
+                        <input className = 'form-text text-dark' onChange = {handleChangeFirst} placeholder = 'First name goes here' value = {first}/>
+                    </div>
+
+
+                    <div className = 'label'>
+                        <label><strong>Family name:</strong></label>
+                    </div>
+                    <div>
+                        <input className = 'form-text text-dark' onChange = {handleChangeLast} placeholder = 'Last name goes here' value = {last}/>
+                    </div>
+
+
+                    <div className = 'label'>
+                        <label><strong>Date of birth:</strong></label>
+                    </div>
+                    <div>
+                        <input className = 'form-text text-dark' onChange = {handleChangeDoB} placeholder = 'yyyy-mm-dd' value = {DoB}/>
+                    </div>
+
+
+                    <div className = 'label'>
+                        <label><strong>Date of death:</strong></label>
+                    </div>
+                    <div>
+                        <input className = 'form-text text-dark' onChange = {handleChangeDoD} placeholder = 'yyyy-mm-dd' value = {DoD}/>
+                    </div>
+
+
+                    <div className = 'submit-btn'>
+                            <button type = 'Submit' className = 'btn' style = {{backgroundColor: 'mediumslateblue', color: 'white'}}>Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
         
     );
