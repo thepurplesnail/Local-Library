20e13254d28309f7b6f0e0c9f4547175efdee05e
thepurplesnail <vanessaman446@gmail.com>
2022-01-27 14:21:04 -0500

    updated book controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,6 +4,7 @@ var Author = require('../models/author');
 var Genre = require('../models/genre');
 var BookInstance = require('../models/bookinstance');
 var async = require('async');
+var {check, validationResult} = require('express-validator')
 
 const sequelize = require('../database'); 
 Promise.resolve().then(sequelize.auth());
@@ -83,29 +84,47 @@ exports.book_detail = function(req, res) {
     ).catch(err => console.log(err));
 };
 
-// Display book create form on GET.
-exports.book_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book create GET');
-};
-
 // Handle book create on POST.
-exports.book_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book create POST');
-};
+exports.book_create_post = [
+    check('title', 'Title is requiried!').trim().isLength({min: 1}).escape(),
 
-// Display book delete form on GET.
-exports.book_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete GET');
-};
+    check('summary').trim()
+        .isLength({min: 1}).withMessage('Summary is required!').escape(),
+
+    check('isbn', 'ISBN is required!').trim().isLength({min: 1}).escape(),
+
+    check('author').trim()
+        .isLength({min: 1}).withMessage('Author name is required!').escape(),
+
+    check('title').custom(val  => 
+        {return Book.findOne({where: {'title': val}})
+        .then(val => 
+            {if (val) return Promise.reject('Book already exists :(').catch(console.log)}
+            )
+        }
+    ), 
+    async (req, res) => {
+        
+        const err = validationResult(req);
+        let bookDetails = {
+            title: req.body.title,
+            summary: req.body.summary,
+            isbn: req.body.isbn,
+            authorId: req.body.author.id,
+        };
+        if (!err.isEmpty()) res.json(err);
+        else { 
+            let book = Book.create(bookDetails).catch(console.log);
+            if (req.body.genres) 
+                for (let g of req.body.genres) book.addGenres(g);
+            console.log(`>>>>> Book: ${bookDetails.title} successfully added!`); 
+        }
+    }
+];
 
 // Handle book delete on POST.
 exports.book_delete_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Book delete POST');
-};
-
-// Display book update form on GET.
-exports.book_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book update GET');
 };
 
 // Handle book update on POST.
