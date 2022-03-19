b8278b62d84a97b66c90f6907232436dde3c80e9
thepurplesnail <vanessaman446@gmail.com>
2022-03-19 12:09:43 -0400

    Update book instance makes PUT request successfully

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -18,6 +18,7 @@ Promise.resolve().then(synchronize());
 // Display list of all BookInstances.
 // GET /catalog/bookinstances
 exports.bookinstance_list = function(req, res) {
+    // find all book instances in DB and their respective book
     BookInstance.findAll({
         include: {model: Book, as: 'book'}
     })
@@ -36,20 +37,25 @@ exports.bookinstance_detail = function(req, res) {
 // Handle BookInstance create on POST.
 // POST /catalog/bookinstance/create
 exports.bookinstance_create_post = [
-        // validate and sanitize fields
+    // validate and sanitize fields
     check('bk.title', 'Book is required!').trim().isLength({min: 1}),
     check('imprint', 'Imprint is required!').trim().isLength({min: 1}),
     check('due_back', 'Invalid date!').optional({checkFalsy: true}).isISO8601().escape(),
     check('status', 'Status is required!').isLength({min: 1}),
     async (req, res) => {
+
         const errors = validationResult(req);
+
+        // create new bookInstance json object
         let bkInstDtls = {
             bookId: req.body.bk.id,
             imprint: req.body.imprint,
             due_back: (req.body.due_back ? req.body.due_back : undefined),
             status: req.body.status
         }
+
         if (!errors.isEmpty()) res.json(errors);
+        // create book if no errors
         else {
             await BookInstance.create(bkInstDtls).catch(console.log);
             console.log(`>>>>>>>>>>>>>>>> Book copy ${req.body.bk.title} successfully created!`);
@@ -62,6 +68,7 @@ exports.bookinstance_create_post = [
 // DELETE /catalog/bookinstance/:id/delete
 exports.bookinstance_delete_post = async function(req, res) {
     try{
+        // find and delete book instance
         await (await BookInstance.findByPk(req.params.id)).destroy();
         res.json('Book instance successfully deleted!');
     } catch (err){
@@ -70,7 +77,33 @@ exports.bookinstance_delete_post = async function(req, res) {
     }
 };
 
-// Handle bookinstance update on POST.
-exports.bookinstance_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance update POST');
-};
+// Handle bookinstance update on PUT.
+exports.bookinstance_update_post = [
+    // validate and sanitize fields
+    check('bk.title', 'Book is required!').trim().isLength({min: 1}),
+    check('imprint', 'Imprint is required!').trim().isLength({min: 1}),
+    check('due_back', 'Invalid date!').optional({checkFalsy: true}).isISO8601().escape(),
+    check('status', 'Status is required!').isLength({min: 1}),
+    async (req, res) => {
+
+        const errors = validationResult(req);
+
+        // create new bookInstance json object
+        let bkInstDtls = {
+            bookId: req.body.bk.id,
+            imprint: req.body.imprint,
+            due_back: (req.body.due_back ? req.body.due_back : undefined),
+            status: req.body.status
+        }
+
+        if (!errors.isEmpty()) res.json(errors);
+        // create book if no errors
+        else {
+            let bookInstance = await BookInstance.findByPk(req.params.id);
+            bookInstance.set(bkInstDtls);
+            await bookInstance.save();
+            console.log(`>>>>>>>>>>>>>>>> Book copy for ${req.body.bk.title} successfully updated!`);
+            res.json(`Copy for ${req.body.bk.title} has been updated!`);
+        }
+    }
+];
--- routes/catalog.js
+++ routes/catalog.js
@@ -69,8 +69,8 @@ router.post('/bookinstance/create', book_instance_controller.bookinstance_create
 // DELETE request to delete BookInstance.
 router.delete('/bookinstance/:id/delete', book_instance_controller.bookinstance_delete_post);
 
-// POST request to update BookInstance.
-router.post('/bookinstance/:id/update', book_instance_controller.bookinstance_update_post);
+// PUT request to update BookInstance.
+router.put('/bookinstance/:id/update', book_instance_controller.bookinstance_update_post);
 
 // GET request for one BookInstance.
 router.get('/bookinstance/:id', book_instance_controller.bookinstance_detail);
